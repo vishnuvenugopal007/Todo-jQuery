70f5986cb1ee5a3c28efcc8303887ee31c2997be
vishnuvenugopal007 <vishnu.venugopal@live.com>
2017-06-02 10:25:42 -0700

    User can add a todo item to list

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,13 +1,24 @@
+/* global $ */
 // Check off Specific Todos by Clicking
-$('li').click(function(){
+$('ul').on('click', 'li', function(){
   $(this).toggleClass('completed')
 })
 
 //Click on X to Delete Todos
 
-$('span').click(function(){
+$('ul').on('click', 'span', function(){
   $(this).parent().fadeOut(500, function(){
     $(this).remove()
   })
   event.stopPropagation()
+})
+
+$('input[type = text]').keypress(function(event){
+  if(event.which === 13){
+    //saving the new todo text to a variable
+    var todoText = $(this).val()
+    $(this).val('')
+    //creating a new li and adding it to the ul
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>')
+  }
 })
