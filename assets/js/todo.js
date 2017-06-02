b2708821ccacb2c0ed5f5d07d702f24dff87919e
vishnuvenugopal007 <vishnu.venugopal@live.com>
2017-06-02 09:56:36 -0700

    User can Delete Todos

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,3 +2,12 @@
 $('li').click(function(){
   $(this).toggleClass('completed')
 })
+
+//Click on X to Delete Todos
+
+$('span').click(function(){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove()
+  })
+  event.stopPropagation()
+})
